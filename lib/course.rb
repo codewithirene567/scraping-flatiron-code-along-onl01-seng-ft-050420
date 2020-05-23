fa69c893d09ebc6e8d3ea5a77382f8c9511ed705
Irene Scott <irenejinscott@gmail.com>
2020-05-23 15:58:30 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,18 @@
+#purpose of our scrape is to get data to assign to the attributes of course class instances
+class Course
+  attr_accessor :title, :schedule, :description
+  @@all = []
 
+  def initialize
+    @@all << self
+  end
 
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+  
+end
