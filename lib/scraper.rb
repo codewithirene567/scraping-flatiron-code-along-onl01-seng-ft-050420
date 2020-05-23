4d0b612cbc7c3c954334570fb8f8033f9c137990
Irene Scott <irenejinscott@gmail.com>
2020-05-23 16:00:33 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,13 +1,29 @@
 require 'nokogiri'
 require 'open-uri'
-require 'pry'
-#must require these up top
-require_relative './course.rb' #we are requiring our couse class file so that the scraer can make new courses
-
+ 
+require_relative './course.rb'
+ 
 class Scraper
-
+ 
+  def get_page
+    Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+  end
+ 
+  def get_courses
+    self.get_page.css(".post")
+  end
+ 
+  def make_courses
+    self.get_courses.each do |post|
+      course = Course.new
+      course.title = post.css("h2").text
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text
+    end
+  end
+ 
   def print_courses
-   self.make_courses
+    self.make_courses
     Course.all.each do |course|
       if course.title && course.title != ""
         puts "Title: #{course.title}"
@@ -16,31 +32,7 @@ class Scraper
       end
     end
   end
-
-  def get_page
-  doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-#doc.css(".post").first.css("(this is where the class selector goes like h2 or date)").text #select the post class using a css selector with the .css and take the whole thing from
-#variable which is doc which is set to the html link
-
-#doc.css(".post").each do |post|
-      #course = Course.new #making a new instance of the course class
-    #  course.title = post.css("h2").text #give that instnace a title for the h2 text, web development immerseive
-    #  course.schedule = post.css(".date").text
-    #  course.description = post.css("p").text
-    #  binding.pry
-    #   end
-  end
-
-  def get_courses
-    self.get_page.css(".post")
-  end
-
-  def make_courses
-  self.get_courses.each do |post|
-    course = Course.new
-    course.title = post.css("h2").text
-    course.schedule = post.css(".date").text
-    course.description = post.css("p").text
-  end
-  end
+ 
 end
+ 
+Scraper.new.print_courses
